cdc522a2e90ed07bd4508791b3b3029d554e921e
Natalonsop <natalonsop@gmail.com>
2023-07-08 18:46:24 -0500

    cuadro item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
-        <h1>cuadros chiquitos</h1> <!-- un cuadrito sin botones :3 --> 
+        <table>
+            <tbody>
+                <tr>
+                    <td class = "s-img" colspan="2"><img src="../assets/Images/doggo.gif"></td>
+                </tr>
+                <tr>
+                    <td colspan="2">Iphone 14 pro ma</td>
+                </tr>
+                <tr>
+                    <td colspan="2">5000</td>
+                </tr>
+                <tr>
+                    <td>15</td>
+                    <td>
+                        <input 
+                        class="submit" 
+                        type="submit" 
+                        value="Guardar">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -10,6 +31,27 @@
   }
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+table , td, th {
+	border: 1px solid #595959;
+	border-collapse: collapse;
+}
+td, th {
+	padding: 1px;
+	width: auto;
+	height: 1px;
+}
+.s-img{
+    width: 200px;
+	height: 200px;
+}
+th {
+	background: #f0e6cc;
+}
+.even {
+	background: #fbf8f0;
+}
+.odd {
+	background: #fefcf9;
+}
 </style>
