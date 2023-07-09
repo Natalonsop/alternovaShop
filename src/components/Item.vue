533d4c7fc91a5e245d22d0c720d1458b9af749aa
Natalonsop <natalonsop@gmail.com>
2023-07-08 20:37:49 -0500

    bootstrap e icono al boton de item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,8 +1,12 @@
+
 <template>
+    
     <div>
+        
         <table>
             <tbody>
                 <tr>
+                    
                     <td class = "s-img" colspan="2"><img src="../assets/Images/doggo.gif"></td>
                 </tr>
                 <tr>
@@ -14,10 +18,11 @@
                 <tr>
                     <td>15</td>
                     <td>
-                        <input 
-                        class="submit" 
-                        type="submit" 
-                        value="Guardar">
+                    <div class="input-group">
+                        <button class="btn btn-primary" type="on-submit" @click="handleClick" >
+                            <i class="bi bi-cart-plus-fill"></i>
+                        </button>
+                    </div>
                     </td>
                 </tr>
             </tbody>
@@ -32,6 +37,8 @@
 </script>
 
 <style>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css");
+
 table , td, th {
 	border: 1px solid #595959;
 	border-collapse: collapse;
